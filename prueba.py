b21378aeb8a127845981504c99cdcb58d4cd414c
arcargotte <alan.argotte69@gmail.com>
2024-03-16 00:47:35 -0400

    "Progreso 1 al dia 15-03"

--- prueba.py
+++ prueba.py
@@ -1,98 +1,72 @@
 import tkinter as tk
-from typing import List
+from typing import List, Tuple, Callable, Union
 
+M : int = 500 #pixeles del tablero
 N = int(input("Diga la dimensión de su tablero: "))
-M : int = 500
-
-ventana = tk.Tk() #siempre debe haber ventana
-ventana.geometry("1200x600")
-tablero = tk.Canvas(ventana, width = 500, height = 500)
+turno: int = 1          
 
 class Casilla:
     def __init__(self,
-                 lienzo : tk.Canvas, 
-                 esquina_superior : List[int],
-                 lado : int,
-                 estado : int):
-        
-        self.lienzo = tablero
-        self.esquina_superior = [0,0]
-        self.lado = N/M
-        self.contador = 0
+                 tablero : tk.Canvas, 
+                 posicion : Tuple[int, int]):
+        self.tablero = tablero
+        self.lado: int = M/N
+        self.lienzo: tk.Canvas = tablero
+        self.esq_superior_izq: Tuple[int, int] = posicion
+        self.estado: int = 0 #estado varia entre 0,1 y 2 para indicar vacio, cruz y circunferencia, respectivamente.
         self.dibujar_casilla()
-    
+
+    def crear_cruz(self):
+        global turno
+        self.tablero.create_line(self.esq_superior_izq[0] + self.lado / 5, self.esq_superior_izq[1] + self.lado / 5, 
+                            self.esq_superior_izq[0] + self.lado * (4/5), self.esq_superior_izq[1] + self.lado * (4/5), fill = "black", width = self.lado / 5)
+        self.tablero.create_line(self.esq_superior_izq[0] + self.lado * (4/5), self.esq_superior_izq[1] + self.lado / 5,
+                            self.esq_superior_izq[0] + self.lado / 5, self.esq_superior_izq[1] + self.lado * (4/5), fill = "black", width = self.lado / 5)
+        self.estado = 1
+        turno = 2
+
+    def crear_circulo(self):
+        global turno
+        self.tablero.create_oval(self.esq_superior_izq[0] + self.lado / 5, self.esq_superior_izq[1] + self.lado / 5, 
+                            self.esq_superior_izq[0] + self.lado * (4/5), self.esq_superior_izq[1] + self.lado * (4/5), fill = "red")
+        self.estado = 2
+        turno = 1
+
     def dibujar_casilla(self):
-        self.casilla = self.lienzo.create_rectangle(self.esquina_superior[0], 
-                                                    self.esquina_superior[1], 
-                                                    self.esquina_superior[0] + self.lado, 
-                                                    self.esquina_superior[1] + self.lado, 
-                                                    fill = "white")
-        
-        if self.contador < N:
-            self.esquina_superior[0] = self.esquina_superior[0] + N
-            self.contador += 1
-        else: 
-            self.esquina_superior[0] = self.esquina_superior[0] + N
-            self.esquina_superior[1] = self.esquina_superior[1] + N
-            self.contador = 0
+        global turno
+        casilla: int = self.tablero.create_rectangle(self.esq_superior_izq[0], self.esq_superior_izq[1], self.esq_superior_izq[0] + self.lado, self.esq_superior_izq[1] + self.lado, fill="white", outline="black")
+        self.tablero.tag_bind(
+            casilla,
+            "<Button-1>",
+            lambda event: ((self.crear_cruz() if (turno == 1) else self.crear_circulo()) if (self.estado == 0) else print("No puedes hacer esto."))) #cambiar esto en un futuro
 
+class Tablero:
+    def __init__(self, raiz, M):
+        self.tablero: tk.Canvas = tk.Canvas(raiz, width = M, height = M, background="black")
+        self.tablero.place(x = 433, y = 144)
+        self.tablero.update()
 
-# class Casillas_N_en_Raya:
-#     def __init__():
+        self.estado: bool = False #False si está jugando, True si está ganado
+        self.lado: int = M
+        self.casillas: List[List[Casilla]] = []
+        self.dibujar_tablero()
 
-tablero.place(x = 433, y = 114)
-rect_00 = tablero.create_rectangle(0,0,100,100, activeoutline= "green" , fill="white")
-rect_01 = tablero.create_rectangle(100,0,200,100, activeoutline= "green" , fill="white")
-rect_02 = tablero.create_rectangle(200,0,300,100, activeoutline= "green" , fill="white")
-rect_03 = tablero.create_rectangle(300,0,400,100, activeoutline= "green" , fill="white")
-rect_04 = tablero.create_rectangle(400,0,500,100, activeoutline= "green" , fill="white")
+    def dibujar_tablero (self):
+        j: int = 0
+        while (j < N):
+            self.casillas.append([])
+            i: int = 0
+            while (i < N):
+                self.casillas[j].append(Casilla(self.tablero, (i * (self.lado / N), j * (self.lado / N))))
+                i += 1
+            j += 1
 
-def crear_cruz(event):
-    tablero.create_line(20,20,80,80, fill = "red", width = 5)
-    tablero.create_line(80,20,20,80, fill = "red", width = 5)
+    # def eliminar_tablero ():
+    # def ganar_tablero ():
+    # def empatar_tablero ():
+    # def reiniciar_tablero ():
 
-def crear_círculo(event):
-    tablero.create_oval(120,20,180,80, fill = "blue")
-
-tablero.tag_bind(
-    rect_00,
-    "<Button-1>",
-    crear_cruz
-
-)
-
-tablero.tag_bind(
-    rect_01,
-    "<Button-1>",
-    crear_círculo
-
-)
-
-
-rect_10 = tablero.create_rectangle(0,100,100,200, activeoutline= "green" , fill="white")
-rect_11 = tablero.create_rectangle(100,100,200,200, activeoutline= "green" , fill="white")
-rect_12 = tablero.create_rectangle(200,100,300,200, activeoutline= "green" , fill="white")
-rect_13 = tablero.create_rectangle(300,100,400,200, activeoutline= "green" , fill="white")
-rect_14 = tablero.create_rectangle(400,100,500,200, activeoutline= "green" , fill="white")
-
-rect_20 = tablero.create_rectangle(0,200,100,300, activeoutline= "green" , fill="white")
-rect_21 = tablero.create_rectangle(100,200,200,300, activeoutline= "green" , fill="white")
-rect_22 = tablero.create_rectangle(200,200,300,300, activeoutline= "green" , fill="white")
-rect_23 = tablero.create_rectangle(300,200,400,300, activeoutline= "green" , fill="white")
-rect_24 = tablero.create_rectangle(400,200,500,300, activeoutline= "green" , fill="white")
-
-rect_30 = tablero.create_rectangle(0,300,100,400, activeoutline= "green" , fill="white")
-rect_31 = tablero.create_rectangle(100,300,200,400, activeoutline= "green" , fill="white")
-rect_32 = tablero.create_rectangle(200,300,300,400, activeoutline= "green" , fill="white")
-rect_33 = tablero.create_rectangle(300,300,400,400, activeoutline= "green" , fill="white")
-rect_34 = tablero.create_rectangle(400,300,500,400, activeoutline= "green" , fill="white")
-
-rect_40 = tablero.create_rectangle(0,400,100,500, activeoutline= "green" , fill="white")
-rect_41 = tablero.create_rectangle(100,400,200,500, activeoutline= "green" , fill="white")
-rect_42 = tablero.create_rectangle(200,400,300,500, activeoutline= "green" , fill="white")
-rect_43 = tablero.create_rectangle(300,400,400,500, activeoutline= "green" , fill="white")
-rect_44 = tablero.create_rectangle(400,400,500,500, activeoutline= "green" , fill="white")
-
-#2x + 500 = 1366             x= 258           
-#2y + 480 = 768              y= 144
-ventana.mainloop()
+raiz = tk.Tk()
+raiz.geometry("1200x600") 
+tablero: Tablero = Tablero(raiz, M)
+raiz.mainloop()
